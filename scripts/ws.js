ba499c421222dc83023b0dba4e79da32a3e539f0
Axsiow <contact@axsiow.tf>
2026-07-22 16:54:01 +0200

    add user-agent and change return code

--- scripts/ws.js
+++ scripts/ws.js
@@ -2,6 +2,7 @@ var url = 'https://{$ENDPOINT}/fdsnws/availability/1/';
 
 try {
     var request = new HttpRequest();
+    request.addHeader('User-agent', 'oculus-monitor');
     var response = request.get(url);
 
     var status = request.getStatus();
@@ -13,5 +14,5 @@ try {
     var error = e.message;
     Zabbix.log(2, 'Error: ' + error);
 
-    return 999;
+    return 504;
 }
