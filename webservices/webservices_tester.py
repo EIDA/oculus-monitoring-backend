4b7fb2b76a58ae15cd94d1e57d38a4a38249dceb
Axsiow <contact@axsiow.tf>
2026-08-31 16:56:20 +0200

    update dep

--- webservices/webservices_tester.py
+++ webservices/webservices_tester.py
@@ -1,5 +1,11 @@
 # /// script
 # requires-python = ">=3.14"
-# dependencies = []
+# dependencies = [
+#     "argparse>=1.4.0",
+#     "logging>=0.4.9.6",
+#     "pathlib>=1.0.1",
+#     "pyyaml>=6.0.3",
+#     "requests>=2.34.2",
+#     "zabbix-utils>=2.0.4",
+# ]
 # ///
-
