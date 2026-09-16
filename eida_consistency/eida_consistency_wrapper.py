63d00e1bcbb0356bc97e717c10191f0f64caeb5f
Axsiow <contact@axsiow.tf>
2026-09-16 16:48:26 +0200

    update eida-consistency to v0.7.0

--- eida_consistency/eida_consistency_wrapper.py
+++ eida_consistency/eida_consistency_wrapper.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = "==3.12"
 # dependencies = [
-#     "eida-consistency==0.6.0",
+#     "eida-consistency==0.7.0",
 #     "zabbix-utils==2.0.4",
 #     "pyyaml",
 #     "setuptools<82",
